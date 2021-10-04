c7ee918222eb528d8067851f076267e900325a8c
Abdirahman Abdi <aabdi005@wch136-24.cs.ucr.edu>
2021-10-03 19:11:07 -0700

    Part 4

--- turnin/aabdi005_lab2_part4.c
+++ turnin/aabdi005_lab2_part4.c
@@ -1,7 +1,7 @@
 /*	Author: aabdi005
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #2  Exercise #1
+ *	Assignment: Lab #2  Exercise #4
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -23,30 +23,29 @@ int main(void) {
 	unsigned char tmpB = 0x00;
 	unsigned char tmpC = 0x00;
 	unsigned char tmpD = 0x00;
-	unsigned short totalWeight = 0x00;
-	
+	unsigned char totalWeight = 0x00;
 
     /* Insert your solution below */
     while (1) {
 	tmpA = PINA;
 	tmpB = PINB;
 	tmpC = PINC;
-
 	totalWeight = tmpA + tmpB + tmpC;
-	if(totalWeight > 140){
-	   tmpD = (tmpD | 0x01);
-	}
-	else {	
-		tmpd = (tmpD | 0x02);
-	}
-	
-	if ((tmpA - tmpC) > 80 || (tmpC - tmpA) > -80) {
-		tmpD = (tmpd | 0x02);
-	}else {
-		tmpD = tmpD | 0x02;
+
+	if( totalWeight > 0x8C){
+	   	tmpD = (tmpD | 0x01);
+	} else {
+	  	tmpD = (tmpD & 0x02);
 	}
 	
-	PORTD = tmpD;	 
+	if (((tmpA - tmpC) > 0x50) | ((tmpC - tmpA) > 0x50)) {
+		tmpD = (tmpD | 0x02);
+	}
+
+
+		totalWeight = totalWeight >> 2;
+		tmpD = (totalWeight & 0xFD) | tmpD;
+		PORTD = tmpD;	 
 	
 	}
     return 0;
